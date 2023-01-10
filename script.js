13ce27d562af200f5d28cb7990744eb80f5a3e1b
RZ-n-f <93051780+RZAlfar@users.noreply.github.com>
2023-01-10 19:21:17 +0200

    front-p-7

---
+++ script.js
@@ -0,0 +1,22 @@
+// const elements = Array.from(document.querySelectorAll("navbar li a"));
+// const nav = document.getElementById("navbarToggleExternalContent");
+// elements.forEach((element) => {
+//   element.addEventListener("click", function (e) {
+//     console.log("ho");
+//     e.setAttribute("href", "#s-1");
+//     e.setAttribute("data-bs-target", "#navbarToggleExternalContent");
+//   });
+// })
+
+
+// navLi.forEach(function (e) {
+//   e.addEventListener("click", function (e) {
+//     console.log("go");
+//     e.style.color = "blue";
+//     // link.setAttribute("data-bs-toggle", "collapse");
+//     // link.setAttribute("data-bs-target", "#navbarToggleExternalContent");
+//   })
+// });
+
+//   data-bs-toggle="collapse"
+            // data-bs-target="#navbarToggleExternalContent"
